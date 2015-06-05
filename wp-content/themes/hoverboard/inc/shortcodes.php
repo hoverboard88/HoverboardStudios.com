95b099a20bec4b350a8824c6b6e2dc4c805bf883
Ryan Tvenge <ryan@tvenge.me>
2015-06-05 11:00:49 -0700

    adding daves search plugin and other design tweaks

--- wp-content/themes/hoverboard/inc/shortcodes.php
+++ wp-content/themes/hoverboard/inc/shortcodes.php
@@ -26,6 +26,8 @@ function hb_shortcode_profile_pictures( $atts ){
 }
 add_shortcode( 'hb_profiles', 'hb_shortcode_profile_pictures' );
 
+// Halves
+
 function hb_shortcode_columns_1( $atts, $content = NULL ){
   //this is a shortterm fix. If you have actual p tags in this shortcode, I think they'll be stripped.
   $content = wpautop(trim($content));
@@ -38,3 +40,24 @@ function hb_shortcode_columns_2( $atts, $content = NULL ){
   return '<div class="column--half column--half--spaced last">' .  do_shortcode($content) . '</div><div class="clear"></div>';
 }
 add_shortcode( 'hb_column_half_2', 'hb_shortcode_columns_2' );
+
+// Thirds
+
+function hb_shortcode_columns_3_1( $atts, $content = NULL ){
+  //this is a shortterm fix. If you have actual p tags in this shortcode, I think they'll be stripped.
+  $content = wpautop(trim($content));
+  return '<div class="column--third column--third--spaced first">' .  do_shortcode($content) . '</div>';
+}
+add_shortcode( 'hb_column_third_1', 'hb_shortcode_columns_3_1' );
+
+function hb_shortcode_columns_3_2( $atts, $content = NULL ){
+  $content = wpautop(trim($content));
+  return '<div class="column--third column--third--spaced">' .  do_shortcode($content) . '</div>';
+}
+add_shortcode( 'hb_column_third_2', 'hb_shortcode_columns_3_2' );
+
+function hb_shortcode_columns_3_3( $atts, $content = NULL ){
+  $content = wpautop(trim($content));
+  return '<div class="column--third column--third--spaced last">' .  do_shortcode($content) . '</div><div class="clear"></div>';
+}
+add_shortcode( 'hb_column_third_3', 'hb_shortcode_columns_3_3' );
