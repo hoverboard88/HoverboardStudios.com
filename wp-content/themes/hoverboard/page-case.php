72396f1b29e8f57d378f9e5c3535c03128319381
Ryan Tvenge <ryan@tvenge.me>
2015-06-05 14:07:09 -0700

    minor styling changes

--- wp-content/themes/hoverboard/page-case.php
+++ wp-content/themes/hoverboard/page-case.php
@@ -20,7 +20,7 @@ get_header(); ?>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 			</header>
 			<p><?php echo get_post_meta( get_the_ID(), 'case-study-summary', true ); ?></p>
-			<a class="btn--purple">Website</a>
+			<a href="<?php echo get_post_meta( get_the_ID(), 'case-study-url', true ); ?>" class="btn--purple">Website</a>
 			<?php echo wp_get_attachment_image( get_post_meta( get_the_ID(), 'case-study-device-image-id', true ), 'full' ); ?>
 		</div>
 	</div>
