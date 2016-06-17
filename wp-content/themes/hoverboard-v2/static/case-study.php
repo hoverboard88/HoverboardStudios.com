50179eac71b2e40304968f11d81ca619a01b2771
Ryan Tvenge <ryan@hoverboardstudios.com>
2016-06-17 09:13:44 -0700

    finished up Case Study

--- wp-content/themes/hoverboard-v2/static/case-study.php
+++ wp-content/themes/hoverboard-v2/static/case-study.php
@@ -4,7 +4,7 @@
   <?php $template_name = basename(__FILE__, '.php'); ?>
   <?php include('inc/head.php'); ?>
 </head>
-<body>
+<body class="page-template-page-case-study-php">
 
   <?php include('inc/search.php'); ?>
 
@@ -17,23 +17,102 @@
   <!-- TODO: possibily switch this out with the <main> tag -->
   <div role="main" class="main main--content">
     <div class="wrap">
-      <div class="container container--page-title container--medium container--page-title">
-        <ul class="list--unstyled list--horizontal list--icons">
-          <li class="icon icon--circle icon--blue"><a href="#"><svg style="width:32px;height:32px" viewBox="0 0 24 24">
-            <path d="M12.2,15.5L9.65,21.72C10.4,21.9 11.19,22 12,22C12.84,22 13.66,21.9 14.44,21.7M20.61,7.06C20.8,7.96 20.76,9.05 20.39,10.25C19.42,13.37 17,19 16.1,21.13C19.58,19.58 22,16.12 22,12.1C22,10.26 21.5,8.53 20.61,7.06M4.31,8.64C4.31,8.64 3.82,8 3.31,8H2.78C2.28,9.13 2,10.62 2,12C2,16.09 4.5,19.61 8.12,21.11M3.13,7.14C4.8,4.03 8.14,2 12,2C14.5,2 16.78,3.06 18.53,4.56C18.03,4.46 17.5,4.57 16.93,4.89C15.64,5.63 15.22,7.71 16.89,8.76C17.94,9.41 18.31,11.04 18.27,12.04C18.24,13.03 15.85,17.61 15.85,17.61L13.5,9.63C13.5,9.63 13.44,9.07 13.44,8.91C13.44,8.71 13.5,8.46 13.63,8.31C13.72,8.22 13.85,8 14,8H15.11V7.14H9.11V8H9.3C9.5,8 9.69,8.29 9.87,8.47C10.09,8.7 10.37,9.55 10.7,10.43L11.57,13.3L9.69,17.63L7.63,8.97C7.63,8.97 7.69,8.37 7.82,8.27C7.9,8.2 8,8 8.17,8H8.22V7.14H3.13Z" />
-          </svg></a></li>
-          <li class="icon icon--circle icon--purple"><a href="#"><svg style="width:32px;height:32px" viewBox="0 0 24 24">
-            <path d="M14.6,16.6L19.2,12L14.6,7.4L16,6L22,12L16,18L14.6,16.6M9.4,16.6L4.8,12L9.4,7.4L8,6L2,12L8,18L9.4,16.6Z" />
-          </svg></a></li>
-        </ul>
-        <h1 class="page-title">About Us</h1>
+      <div class="container container--case-study-title">
+        <div class="container-page-title">
+          <ul class="list--unstyled list--horizontal list--icons">
+            <li class="icon icon--circle icon--blue"><a href="#"><svg style="width:32px;height:32px" viewBox="0 0 24 24">
+              <path d="M12.2,15.5L9.65,21.72C10.4,21.9 11.19,22 12,22C12.84,22 13.66,21.9 14.44,21.7M20.61,7.06C20.8,7.96 20.76,9.05 20.39,10.25C19.42,13.37 17,19 16.1,21.13C19.58,19.58 22,16.12 22,12.1C22,10.26 21.5,8.53 20.61,7.06M4.31,8.64C4.31,8.64 3.82,8 3.31,8H2.78C2.28,9.13 2,10.62 2,12C2,16.09 4.5,19.61 8.12,21.11M3.13,7.14C4.8,4.03 8.14,2 12,2C14.5,2 16.78,3.06 18.53,4.56C18.03,4.46 17.5,4.57 16.93,4.89C15.64,5.63 15.22,7.71 16.89,8.76C17.94,9.41 18.31,11.04 18.27,12.04C18.24,13.03 15.85,17.61 15.85,17.61L13.5,9.63C13.5,9.63 13.44,9.07 13.44,8.91C13.44,8.71 13.5,8.46 13.63,8.31C13.72,8.22 13.85,8 14,8H15.11V7.14H9.11V8H9.3C9.5,8 9.69,8.29 9.87,8.47C10.09,8.7 10.37,9.55 10.7,10.43L11.57,13.3L9.69,17.63L7.63,8.97C7.63,8.97 7.69,8.37 7.82,8.27C7.9,8.2 8,8 8.17,8H8.22V7.14H3.13Z" />
+            </svg></a></li>
+            <li class="icon icon--circle icon--purple"><a href="#"><svg style="width:32px;height:32px" viewBox="0 0 24 24">
+              <path d="M14.6,16.6L19.2,12L14.6,7.4L16,6L22,12L16,18L14.6,16.6M9.4,16.6L4.8,12L9.4,7.4L8,6L2,12L8,18L9.4,16.6Z" />
+            </svg></a></li>
+          </ul>
+          <h1 class="page-title">Standard Distributing</h1>
+          <a href="http://standarddistributing.com" class="link--icon">
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+              <path fill="#000000" d="M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z" />
+            </svg>
+            standarddistributing.com
+          </a>
+        </div>
       </div>
     </div>
+
     <div class="wrap">
-      <div class="content container container--small">
 
+      <div class="container static">
 
+        <div class="portfolio__example">
+          <img src="../dist/img/portfolio_standarddist_desk.png" alt="">
+        </div>
 
+        <div class="content content--case-study">
+
+          <p>Previously, they were using a Front Page-like process, so they had full control of the design. It had served them very well over the years, but they had realized the business outgrew the site’s functionality.</p>
+
+          <h2>Goals</h2>
+          <!-- Will use shortcode columns to make "side by side" -->
+          <h3 class="heading--underlined">Standard Distributing’s Goals</h3>
+
+          <ol>
+            <li>Create a seamless user experience for current customers to log into the ordering system.</li>
+            <li>Ability to add promotions to website.</li>
+            <li>Update site design to reflect new branding.</li>
+          </ol>
+
+          <h3 class="heading--underlined">Hoverboard’s Goals</h3>
+
+          <ol>
+            <li>Give Standard a CMS to update the website while keeping in place the overall design aesthetic.</li>
+            <li>New design to be responsive to prolong the investment of the new site.</li>
+            <li>Keep front-end code as lightweight as possible.</li>
+            <li>Streamline current email campaign process</li>
+          </ol>
+
+          <h2>Design</h2>
+
+          <p>The overall look and feel was based off the supplied branding, using the same color palette and ribbon treatment for headings on the site.</p>
+
+          <p>There wasn’t an equivalent typeface to the branding type so we used “Oswald”, a similar web font in the Google Fonts Library.</p>
+
+          <div class="case-study-sidebar">
+            <figure id="attachment_134" style="width: 600px" class="wp-caption alignright">
+              <img src="http://placehold.it/600x200/" alt="">
+              <figcaption class="wp-caption-text">This is the caption</figcaption>
+            </figure>
+          </div>
+
+          <p><strong>After the discovery process, we concluded there were two different user stories:</strong></p>
+
+          <ul class="list--emphasis">
+            <li>As a potential customer, I am gathering information about Standard Distributing. I would either fill out the contact form or call the phone number to get in touch.</li>
+            <li>As a current customer, I am looking at current promotions or logging into the backend system to place an order.</li>
+          </ul>
+
+          <p>Because of these user stories, we made the “Customer Login”, “Contact” buttons and the (clickable) phone numbe	r prominent in the header.</p>
+
+          <h2>Front-End Code</h2>
+
+          <div class="case-study-sidebar">
+            <figure id="attachment_134" style="width: 300px" class="wp-caption alignright">
+              <img class="size-medium wp-image-134" src="../dist/img/SDBanner.png" alt="Banner of website" width="300" height="123">
+              <figcaption class="wp-caption-text">This is the caption</figcaption>
+            </figure>
+          </div>
+
+          <p>The website is built responsively with a mobile-first approach. Per our goals from above, we kept the front-end as lightweight as possible, using SVG’s and CSS whenever possible.</p>
+
+          <p>Inuit.css was used as a lightweight css framework using BEM naming conventions to keep everything as modular as possible.</p>
+
+          <p>The underline animation on the header navigation ended up being very successful in the community.</p>
+
+          <blockquote>
+            <p>Everyone wants to be heard—your audience included.</p>
+
+            <p>The trouble is how to go about it.</p>
+          </blockquote>
+
+        </div>
       </div>
     </div>
     <div class="wrap">
