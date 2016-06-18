782c9dcc84f8d696d745446a1b14176d857db341
Ryan Tvenge <ryan@hoverboardstudios.com>
2016-06-17 18:51:11 -0700

    finished case studies

--- wp-content/themes/hoverboard-v2/functions.php
+++ wp-content/themes/hoverboard-v2/functions.php
@@ -198,3 +198,8 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+ * Load Shortcodes.
+ */
+require get_template_directory() . '/inc/shortcodes.php';
