a9c94ce005f57010c1c086dc106fc651aaf2a022
Ryan Tvenge <ryan@hoverboardstudios.com>
2016-06-20 17:52:18 -0700

    adding custom taxonomy for case studies

--- wp-content/themes/hoverboard-v2/functions.php
+++ wp-content/themes/hoverboard-v2/functions.php
@@ -158,6 +158,31 @@ function hb_v2_cpts() {
 		)
 	);
 
+	$labels = array(
+    'name' => _x( 'Categories', 'taxonomy general name' ),
+    'singular_name' => _x( 'Category', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Categories' ),
+    'all_items' => __( 'All Categories' ),
+    'parent_item' => __( 'Parent Category' ),
+    'parent_item_colon' => __( 'Parent Category:' ),
+    'edit_item' => __( 'Edit Category' ),
+    'update_item' => __( 'Update Category' ),
+    'add_new_item' => __( 'Add New Category' ),
+    'new_item_name' => __( 'New Category Name' ),
+    'menu_name' => __( 'Categories' ),
+  );
+
+	// create a new taxonomy
+	register_taxonomy(
+		'tech_category',
+		'studies',
+		array(
+			'label' => __( 'Category' ),
+			'hierarchical' => true,
+			'labels' => $labels,
+		)
+	);
+
 }
 add_action( 'init', 'hb_v2_cpts' );
 
