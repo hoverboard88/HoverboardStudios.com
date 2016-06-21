085a6e3e20df0b2c8fa2ce419a3d981aec9f19c0
Ryan Tvenge <ryan@hoverboardstudios.com>
2016-06-21 14:59:53 -0700

    organizing functions.php

--- wp-content/themes/hoverboard-v2/functions.php
+++ wp-content/themes/hoverboard-v2/functions.php
@@ -45,6 +45,7 @@ function hb_v2_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary', 'hb_v2' ),
+		'footer' => esc_html__( 'Footer', 'hb_v2' ),
 	) );
 
 	/*
@@ -59,23 +60,6 @@ function hb_v2_setup() {
 		'caption',
 	) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See https://developer.wordpress.org/themes/functionality/post-formats/
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'image',
-		'video',
-		'quote',
-		'link',
-	) );
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'hb_v2_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
 }
 endif;
 add_action( 'after_setup_theme', 'hb_v2_setup' );
@@ -91,122 +75,6 @@ function hb_v2_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'hb_v2_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'hb_v2_content_width', 0 );
-
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function hb_v2_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'hb_v2' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'hb_v2' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-}
-add_action( 'widgets_init', 'hb_v2_widgets_init' );
-
-/**
- * Enqueue scripts and styles.
- */
-function hb_v2_scripts() {
-	// wp_enqueue_style( 'hb_v2-style', get_template_directory_uri() . '/dist/css/style.css' );
-
-	wp_deregister_script('jquery');
-
-	// footnotes plugin, jquery.tools doesn't work with our version of jquery.
-	wp_dequeue_style('mci-footnotes-css-public');
-	wp_dequeue_script('mci-footnotes-js-jquery-tools');
-
-  wp_register_script('jquery', "//ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js", false, null);
-  wp_enqueue_script('jquery');
-
-	wp_enqueue_script( 'hb_v2-mainjs', get_template_directory_uri() . '/dist/js/main.min.js', array('jquery'), '20151215', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'hb_v2_scripts' );
-
-function hb_v2_wp_footer() {
-	echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/dist/css/style.css" type="text/css" media="all" />';
-}
-add_action( 'wp_footer', 'hb_v2_wp_footer' );
-
-function hb_v2__wp_head() {
-	echo '<style>';
-	include get_stylesheet_directory() . '/dist/css/critical.css';
-	echo '</style>';
-
-	// TypeKit
-	echo '<script> (function(d) { var config = { kitId: "bev1prj", scriptTimeout: 3000, async: true }, h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src="https://use.typekit.net/"+config.kitId+".js";tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s) })(document); </script>';
-}
-add_action( 'wp_head', 'hb_v2__wp_head' );
-
-function hb_v2_cpts() {
-
-	$labels = array(
-    'name' => _x( 'Categories', 'taxonomy general name' ),
-    'singular_name' => _x( 'Category', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search Categories' ),
-    'all_items' => __( 'All Categories' ),
-    'parent_item' => __( 'Parent Category' ),
-    'parent_item_colon' => __( 'Parent Category:' ),
-    'edit_item' => __( 'Edit Category' ),
-    'update_item' => __( 'Update Category' ),
-    'add_new_item' => __( 'Add New Category' ),
-    'new_item_name' => __( 'New Category Name' ),
-    'menu_name' => __( 'Categories' ),
-  );
-
-	// create a new taxonomy
-	register_taxonomy(
-		'tech_category',
-		'studies',
-		array(
-			'label' => __( 'Category' ),
-			'rewrite' => array( 'slug' => 'project-category' ),
-			'hierarchical' => true,
-			'labels' => $labels,
-		)
-	);
-
-
-	register_post_type( 'studies',
-	// CPT Options
-		array(
-			'labels' => array(
-				'name' => __( 'Case Studies' ),
-				'singular_name' => __( 'Case Study' )
-			),
-			'supports' => array( 'excerpt', 'editor', 'title' ),
-			'taxonomies' => array( 'tech_category' ),
-
-			'public' => true,
-			'has_archive' => true,
-			'rewrite' => array('slug' => 'studies'),
-		)
-	);
-
-}
-add_action( 'init', 'hb_v2_cpts' );
-
-function hb_v2_svg($file, $default = '') {
-	if ( file_exists(get_template_directory() . '/dist/img/' . $file) ) {
-		echo file_get_contents(get_template_directory() . '/dist/img/' . $file);
-	} else {
-		echo file_get_contents(get_template_directory() . '/dist/img/' . $default);
-	}
-}
-
-function hb_v2_prettify_url($url) {
-	return preg_replace("/https?:\/\/(.*)/u", "$1", $url);
-}
 
 /**
  * Add classes to post-list for css purposes
@@ -249,52 +117,6 @@ function hb_v2_replace_ellipsis($content) {
 }
 add_filter('the_excerpt', 'hb_v2_replace_ellipsis');
 
-function hb_v2_category_color($queried_object) {
-	$term_id = $queried_object->term_id;
-	return get_field('category-icon-color', get_category($term_id));
-}
-
-function hb_v2_category_icon($queried_object) {
-	$term_id = $queried_object->term_id;
-	return hb_v2_svg('mdi-' . get_category($term_id)->slug . '.svg', 'mdi-default.svg');
-}
-
-function hb_v2_get_featured_study() {
-
-	$args = array(
-		'posts_per_page'   => 1,
-		'orderby'          => 'date',
-		'order'            => 'DESC',
-		'meta_key'         => 'study_index_featured',
-		'meta_value'       => '1',
-		'post_type'        => 'studies',
-		'post_status'      => 'publish'
-	);
-
-	$posts_array = get_posts( $args );
-
-	return $posts_array[0];
-
-}
-
-function hb_v2_get_home_featured_studies() {
-
-	$args = array(
-		'posts_per_page'   => 2,
-		'orderby'          => 'date',
-		'order'            => 'DESC',
-		'meta_key'         => 'study_home_featured',
-		'meta_value'       => '1',
-		'post_type'        => 'studies',
-		'post_status'      => 'publish'
-	);
-
-	$posts_array = get_posts( $args );
-
-	return $posts_array;
-
-}
-
 /**
  * Implement the Custom Header feature.
  */
@@ -304,11 +126,6 @@ require get_template_directory() . '/inc/custom-header.php';
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Create Custom Fields via Advanced Custom Fields plugin
- */
-// require get_template_directory() . '/inc/advanced-custom-fields.php';
 
 /**
  * Custom functions that act independently of the theme templates.
@@ -329,3 +146,23 @@ require get_template_directory() . '/inc/jetpack.php';
  * Load Shortcodes.
  */
 require get_template_directory() . '/inc/shortcodes.php';
+
+/**
+ * Load RSS Feed Customizations
+ */
+require get_template_directory() . '/inc/rss-feeds.php';
+
+/**
+ * Helper Functions
+ */
+require get_template_directory() . '/inc/helper-functions.php';
+
+/**
+ * Styles and Scripts
+ */
+require get_template_directory() . '/inc/styles-scripts.php';
+
+/**
+ * Custom Post Types and such
+ */
+require get_template_directory() . '/inc/post-types.php';
---
+++ wp-content/themes/hoverboard-v2/inc/styles-scripts.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Enqueue scripts and styles.
+ */
+function hb_v2_scripts() {
+	// wp_enqueue_style( 'hb_v2-style', get_template_directory_uri() . '/dist/css/style.css' );
+
+	wp_deregister_script('jquery');
+
+	// footnotes plugin, jquery.tools doesn't work with our version of jquery.
+	wp_dequeue_style('mci-footnotes-css-public');
+	wp_dequeue_script('mci-footnotes-js-jquery-tools');
+
+  wp_register_script('jquery', "//ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js", false, null);
+  wp_enqueue_script('jquery');
+
+	wp_enqueue_script( 'hb_v2-mainjs', get_template_directory_uri() . '/dist/js/main.min.js', array('jquery'), '20151215', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'hb_v2_scripts' );
+
+function hb_v2_wp_footer() {
+	echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/dist/css/style.css" type="text/css" media="all" />';
+}
+add_action( 'wp_footer', 'hb_v2_wp_footer' );
+
+function hb_v2__wp_head() {
+	echo '<style>';
+	include get_stylesheet_directory() . '/dist/css/critical.css';
+	echo '</style>';
+
+	// TypeKit
+	echo '<script> (function(d) { var config = { kitId: "bev1prj", scriptTimeout: 3000, async: true }, h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src="https://use.typekit.net/"+config.kitId+".js";tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s) })(document); </script>';
+}
+add_action( 'wp_head', 'hb_v2__wp_head' );
