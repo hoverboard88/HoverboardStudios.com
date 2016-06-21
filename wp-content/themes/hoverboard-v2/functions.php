16652051b86dcbd3aadf2f8f14eff5d7bb8a7dcb
Ryan Tvenge <ryan@hoverboardstudios.com>
2016-06-20 21:07:22 -0700

    adding design icon, working on case study

--- wp-content/themes/hoverboard-v2/functions.php
+++ wp-content/themes/hoverboard-v2/functions.php
@@ -152,36 +152,38 @@ function hb_v2_cpts() {
 				'name' => __( 'Case Studies' ),
 				'singular_name' => __( 'Case Study' )
 			),
+			'taxonomies' => array( 'category' ),
+
 			'public' => true,
 			'has_archive' => true,
 			'rewrite' => array('slug' => 'studies'),
 		)
 	);
 
-	$labels = array(
-    'name' => _x( 'Categories', 'taxonomy general name' ),
-    'singular_name' => _x( 'Category', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Search Categories' ),
-    'all_items' => __( 'All Categories' ),
-    'parent_item' => __( 'Parent Category' ),
-    'parent_item_colon' => __( 'Parent Category:' ),
-    'edit_item' => __( 'Edit Category' ),
-    'update_item' => __( 'Update Category' ),
-    'add_new_item' => __( 'Add New Category' ),
-    'new_item_name' => __( 'New Category Name' ),
-    'menu_name' => __( 'Categories' ),
-  );
+	// $labels = array(
+  //   'name' => _x( 'Categories', 'taxonomy general name' ),
+  //   'singular_name' => _x( 'Category', 'taxonomy singular name' ),
+  //   'search_items' =>  __( 'Search Categories' ),
+  //   'all_items' => __( 'All Categories' ),
+  //   'parent_item' => __( 'Parent Category' ),
+  //   'parent_item_colon' => __( 'Parent Category:' ),
+  //   'edit_item' => __( 'Edit Category' ),
+  //   'update_item' => __( 'Update Category' ),
+  //   'add_new_item' => __( 'Add New Category' ),
+  //   'new_item_name' => __( 'New Category Name' ),
+  //   'menu_name' => __( 'Categories' ),
+  // );
 
 	// create a new taxonomy
-	register_taxonomy(
-		'tech_category',
-		'studies',
-		array(
-			'label' => __( 'Category' ),
-			'hierarchical' => true,
-			'labels' => $labels,
-		)
-	);
+	// register_taxonomy(
+	// 	'tech_category',
+	// 	'studies',
+	// 	array(
+	// 		'label' => __( 'Category' ),
+	// 		'hierarchical' => true,
+	// 		'labels' => $labels,
+	// 	)
+	// );
 
 }
 add_action( 'init', 'hb_v2_cpts' );
