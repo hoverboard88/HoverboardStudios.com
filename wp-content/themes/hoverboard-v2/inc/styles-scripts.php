36b520138fdcf1eb6e170f27c555f90ff5236991
Ryan Tvenge <ryan@hoverboardstudios.com>
2016-06-22 09:16:29 -0700

    moving scripts and stuff to footer

--- wp-content/themes/hoverboard-v2/inc/styles-scripts.php
+++ wp-content/themes/hoverboard-v2/inc/styles-scripts.php
@@ -7,6 +7,15 @@ function hb_v2_scripts() {
 
 	wp_deregister_script('jquery');
 
+  //plugin for some reason is enqueuing style on front-end.
+	wp_dequeue_style('wpt-twitter-feed');
+
+	//don't need CF7 styles. Gonna write our own.
+	wp_deregister_style('contact-form-7');
+
+	// dont need their styles either
+	wp_dequeue_style('prism-detached');
+
 	// footnotes plugin, jquery.tools doesn't work with our version of jquery.
 	wp_dequeue_style('mci-footnotes-css-public');
 	wp_dequeue_script('mci-footnotes-js-jquery-tools');
@@ -24,6 +33,8 @@ add_action( 'wp_enqueue_scripts', 'hb_v2_scripts' );
 
 function hb_v2_wp_footer() {
 	echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/dist/css/style.css" type="text/css" media="all" />';
+	// TypeKit
+	echo '<script> (function(d) { var config = { kitId: "bev1prj", scriptTimeout: 3000, async: true }, h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src="https://use.typekit.net/"+config.kitId+".js";tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s) })(document); </script>';
 }
 add_action( 'wp_footer', 'hb_v2_wp_footer' );
 
@@ -32,7 +43,5 @@ function hb_v2__wp_head() {
 	include get_stylesheet_directory() . '/dist/css/critical.css';
 	echo '</style>';
 
-	// TypeKit
-	echo '<script> (function(d) { var config = { kitId: "bev1prj", scriptTimeout: 3000, async: true }, h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src="https://use.typekit.net/"+config.kitId+".js";tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s) })(document); </script>';
 }
 add_action( 'wp_head', 'hb_v2__wp_head' );
