6f41b0ea9dc406b6f12b4de88eca6fb75c7ca41c
Ryan Tvenge <ryan@hoverboardstudios.com>
2016-06-24 18:00:37 -0700

    moving lightbox css to footer

--- wp-content/themes/hoverboard-v2/inc/styles-scripts.php
+++ wp-content/themes/hoverboard-v2/inc/styles-scripts.php
@@ -20,6 +20,10 @@ function hb_v2_scripts() {
 	wp_dequeue_style('mci-footnotes-css-public');
 	wp_dequeue_script('mci-footnotes-js-jquery-tools');
 
+  wp_dequeue_style('slb_core');
+	wp_deregister_style('slb_core');
+
+
   wp_register_script('jquery', "//ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js", false, null);
   wp_enqueue_script('jquery');
 
@@ -28,11 +32,13 @@ function hb_v2_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
 }
 add_action( 'wp_enqueue_scripts', 'hb_v2_scripts' );
 
 function hb_v2_wp_footer() {
 	echo '<link rel="stylesheet" href="' . get_template_directory_uri() . '/dist/css/style.css" type="text/css" media="all" />';
+  echo '<link rel="stylesheet" href="/wp-content/plugins/simple-lightbox/client/css/app.css?ver=2.5.3" type="text/css" media="all">';
 	// TypeKit
 	echo '<script> (function(d) { var config = { kitId: "bev1prj", scriptTimeout: 3000, async: true }, h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src="https://use.typekit.net/"+config.kitId+".js";tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s) })(document); </script>';
 }
